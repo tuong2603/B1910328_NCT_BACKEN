b3d8fdbc3c8edf8ed79a3467c5cf81b1eae6ccd6
b1910314 <tuongb1910328@student.ctu.edu.vn>
2023-02-19 00:33:34 +0700

    Cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcom to contact book application."});
+});
+module.exports = app;
